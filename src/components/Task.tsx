509edb155c70bc5287aac3a4093ed9b03777d888
informatica10 <alexteotonio@bioextratus.rede.bioextratus>
2023-05-06 14:48:03 -0300

    Finishing application

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,13 +1,59 @@
+import { useState, ChangeEvent, MouseEventHandler} from 'react'
 import styles from './Task.module.css'
 import plusLogo from '../assets/plus.svg'
+import trash from '../assets/trash.svg'
 import { NoTask } from './NoTask'
+
+
+type Task = {
+  text: string;
+  isChecked: boolean;
+}
+
 export function Task() {
+  const [newTask, setNewTask] = useState('')
+  const [tasks, setTasks] = useState<Array<Task>>([]);
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTask(event.target.value);
+  }
+
+  function handleCreateNewTask(){
+    const newTaskObject: Task = {
+      text: newTask,
+      isChecked: false,
+    };
+    setTasks([...tasks, newTaskObject]);
+    setNewTask('');
+  }
+
+  function handleDeleteTask(task: Task){
+    const newTasks = tasks.filter((t)=>{
+      return t !== task
+    });
+    setTasks(newTasks)
+  }
+
+  function handleToggleTask(task: Task) {
+    const newTasks = tasks.map((t) => {
+      if (t === task) {
+        return {
+          ...t,
+          isChecked: !t.isChecked,
+        };
+      } else {
+        return t;
+      }
+    });
+    setTasks(newTasks);
+  }
+
 
   return (
     <div className={styles.container}>
       <div className={styles.areaInput}>
-        <input type="text" placeholder='Adicione uma nova tarefa' />
-          <button className={styles.button}>
+        <input type="text" placeholder='Adicione uma nova tarefa' value={newTask} onChange={handleNewTaskChange} />
+          <button className={styles.button} onClick={handleCreateNewTask}>
             Criar <img src={plusLogo}/>
           </button>
       </div>
@@ -16,14 +62,31 @@ export function Task() {
         <header className={styles.headerContent}>
           <div className={styles.headerControlTasks}>
             <span className={styles.labelTarefasCriadas}>Tarefas criadas</span>
-            <small>0</small>
+            <small>{tasks.length}</small>
           </div>
           <div className={styles.headerControlTasks}>
             <span className={styles.labelTarefasConcluidas}>Concluídas</span>
-            <small>0</small>
+            <small>{tasks.filter((t) => t.isChecked).length}</small>
           </div>
         </header>
-          <NoTask/>
+        
+          { tasks.map((task) => (
+            <main key={task.text} className={styles.taskList}>
+              <input 
+                type="checkbox"
+                className={styles.doneTask}
+                onChange={() => handleToggleTask(task)}
+                checked={task.isChecked}
+              />
+              
+              <span>
+                {task.isChecked? <s>{task.text}</s> : task.text}
+              </span>
+              <button onClick={() => handleDeleteTask(task)}>
+                <img src={trash}/>
+              </button>
+            </main>
+          ))}
       </div>
     </div>
   )
